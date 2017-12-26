130a331eb8fdfbb604fac6456c1534d4553b5bd4
qholness <qholness@gmail.com>
2017-12-25 21:39:43 -0800

    Catching a few more errors. Adding a few tests

---
+++ Chef/dishes.py
@@ -0,0 +1,11 @@
+dishes = set([
+"teapot",
+"pressure cooker",
+"cast iron skillet",
+"skillet",
+"waffle maker",
+"prep bowl"
+])
+
+serving_dishes = set([
+	"pitcher"])
--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-python ../Chef/main.py hello_world.chef
+python ../Chef/__init__.py hello_world.chef
